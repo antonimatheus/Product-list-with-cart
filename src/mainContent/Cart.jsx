e9908b99346f44976a078c789e543b5ae7751a77
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2024-07-18 17:26:02 -0400

    added a line to split the stuffs

--- src/mainContent/Cart.jsx
+++ src/mainContent/Cart.jsx
@@ -12,24 +12,27 @@ function Cart() {
             </div>
             <div className="cart--Order">
                 <div className="cart--Order--info">
-                    <div className="cart--Order--h1">
-                        <h2>Classic Tiramisu</h2>
+                    <div>
+                        <div className="cart--Order--h1">
+                            <h2>Classic Tiramisu</h2>
+                        </div>
+                        <div className="cart--Order--quantity">
+                            <div className="cart--Order--quantityh3">
+                                <h3>1x</h3>
+                            </div>
+                            <div className="cart--Order--quantityp">
+                                <p>@ 5.50</p>
+                            </div>
+                            <div className="cart--Order--quantitym">
+                                <p>$5.50</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="cart--Order--quantity">
-                        <div className="cart--Order--quantityh3">
-                            <h3>1x</h3>
-                        </div>
-                        <div className="cart--Order--quantityp">
-                            <p>@ 5.50</p>
-                        </div>
-                        <div className="cart--Order--quantitym">
-                            <p>$5.50</p>
-                        </div>
+                    <div className="cart--Order--delete">
+                        <p> <img src={removeItem} alt="Remove Item" /> </p>
                     </div>
                 </div>
-                <div className="cart--Order--delete">
-                    <p> <img src={removeItem} alt="Remove Item" /> </p>
-                </div>
+                
                 <div className="cart--Line">
                     <span></span>
                 </div>
