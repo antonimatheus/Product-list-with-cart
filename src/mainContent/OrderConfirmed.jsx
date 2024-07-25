9f85d04a99b22963b5a94129237a6f9cf66ff967
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2024-07-25 15:21:02 -0400

    fixed OrderConfirmed size

--- src/mainContent/OrderConfirmed.jsx
+++ src/mainContent/OrderConfirmed.jsx
@@ -15,49 +15,51 @@ function OrderConfirmed({ cartItems }) {
     };
 
     return (
-        <div className="order-confirmed">
-            <img src={confirmed} alt="confirmed" />
-            <div>
-                <h1>Order Confirmed</h1>
-                <p>We hope you enjoy your food!</p>
-            </div>
-            <div>
-                {cartItems.map((item, index) => (
-                    <div key={index} className="cart--OrderConfirmed">
-                        <div className="cart--Order--infoConfirmed">
-                            <div className="cart--Order--infoConfirmedIMGLIST">
-                                <div>
-                                    <img src={item.image.thumbnail} alt={item.name} />
-                                </div>
-                                <div>
-                                    <div className="cart--Order--h1">
-                                        <h2>{item.name}</h2>
-                                    </div>
-                                    <div className="cart--Order--quantity">
-                                        <div className="cart--Order--quantityh3">
-                                            <h3>{item.quantity}x</h3>
-                                        </div>
-                                        <div className="cart--Order--quantityp">
-                                            <p>@ ${item.price.toFixed(2)}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="cart--Order--quantitym">
-                                <p>${(item.price * item.quantity).toFixed(2)}</p>
-                            </div>
-                        </div>
-                        <div className="cart--Line">
-                            <span></span>
-                        </div>
-                    </div>
-                ))}
-                <div className="cart--Total--confirmed">
-                    <p>Order Total</p>
-                    <h2>${calculateTotal()}</h2>
+        <div className="order-confirmed--container">
+            <div className="order-confirmed">
+                <img src={confirmed} alt="confirmed" />
+                <div>
+                    <h1>Order Confirmed</h1>
+                    <p>We hope you enjoy your food!</p>
                 </div>
+                <div>
+                    {cartItems.map((item, index) => (
+                        <div key={index} className="cart--OrderConfirmed">
+                            <div className="cart--Order--infoConfirmed">
+                                <div className="cart--Order--infoConfirmedIMGLIST">
+                                    <div>
+                                        <img src={item.image.thumbnail} alt={item.name} />
+                                    </div>
+                                    <div>
+                                        <div className="cart--Order--h1">
+                                            <h2>{item.name}</h2>
+                                        </div>
+                                        <div className="cart--Order--quantity">
+                                            <div className="cart--Order--quantityh3">
+                                                <h3>{item.quantity}x</h3>
+                                            </div>
+                                            <div className="cart--Order--quantityp">
+                                                <p>@ ${item.price.toFixed(2)}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="cart--Order--quantitym">
+                                    <p>${(item.price * item.quantity).toFixed(2)}</p>
+                                </div>
+                            </div>
+                            <div className="cart--Line">
+                                <span></span>
+                            </div>
+                        </div>
+                    ))}
+                    <div className="cart--Total--confirmed">
+                        <p>Order Total</p>
+                        <h2>${calculateTotal()}</h2>
+                    </div>
+                </div>
+                <button onClick={handleNewOrder}>Start New Order</button>
             </div>
-            <button onClick={handleNewOrder}>Start New Order</button>
         </div>
     );
 }
